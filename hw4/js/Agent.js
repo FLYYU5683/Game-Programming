be27cd5a5a8558fb80dccf9544f4023a23955e2b
FLYYU5683 <woody90514@gmail.com>
2022-05-17 05:22:33 +0800

    hw4

--- hw4/js/Agent.js
+++ hw4/js/Agent.js
@@ -1,4 +1,4 @@
-import * as THREE from 'http://cdn.skypack.dev/three@0.136';
+import * as THREE from 'https://cdn.skypack.dev/three@0.136';
 import {scene} from './Init.js';
 function clamp(a,b,c){
 	{return Math.max(b,Math.min(c,a));}
